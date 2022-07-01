9b7711356ef8d6ca32b47c4ecff36f1ecb53bc6b
Fabricio Miranda <fabricio@itexto.com.br>
2022-07-01 17:31:19 -0300

    spring security, autenticação form login

--- src/main/java/br/com/alura/forum/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/security/SecurityConfiguration.java
@@ -1,14 +1,21 @@
 package br.com.alura.forum.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
 
     // Configuração de autorização de acesso aos endpoints
     @Override
@@ -19,6 +26,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
                 //forma de autienticação
                 .anyRequest().authenticated().and().formLogin();
-        ;
     }
+
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$RSUIYaLzQiQspEG2hM7Kr.y6c7VXmxTDQmmuiT7K6YZp1HzAqTnUW');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
